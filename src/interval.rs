635386a27887520e0b9265ab568eee438a180055
marwterzi <el21190@mail.ntua.gr>
2026-05-21 02:12:50 +0300

    helper functions

---
+++ src/interval.rs
@@ -0,0 +1,35 @@
+pub struct Interval {
+    pub min: f64,
+    pub max: f64,
+}
+
+impl Interval {
+    pub fn new_empty() -> Self {
+        Self { min: f64::INFINITY, max: f64::NEG_INFINITY }
+    }
+
+    pub fn new(min: f64, max: f64) -> Self {
+        Self { min, max }
+    }
+
+    pub fn size(&self) -> f64 {
+        self.max - self.min
+    }
+
+    pub fn contains(&self, x: f64) -> bool {
+        self.min <= x && x <= self.max
+    }
+
+    pub fn surrounds(&self, x: f64) -> bool {
+        self.min < x && x < self.max
+    }
+
+    pub fn clamp(&self, x: f64) -> f64 {
+        if x < self.min { return self.min; }
+        if x > self.max { return self.max; }
+        x
+    }
+
+    pub const EMPTY:    Self = Self { min:  f64::INFINITY,     max: f64::NEG_INFINITY };
+    pub const UNIVERSE: Self = Self { min:  f64::NEG_INFINITY, max: f64::INFINITY     };
+}
